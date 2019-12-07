d9b11a71dc6573d44c35f803e0db4ea424743abc
Tashila Fernando <tashila97@hotmail.com>
2019-12-07 13:59:47 +0530

    Init commit

---
+++ build/precache-manifest.d99de675313312f02b6355664018b222.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d21509d539703e75889c921dc0eca0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de82f2fc4eb701812cf7",
+    "url": "/static/css/main.a7bfd8d7.chunk.css"
+  },
+  {
+    "revision": "d15bd96a5257917ce322",
+    "url": "/static/js/2.21c8f38e.chunk.js"
+  },
+  {
+    "revision": "de82f2fc4eb701812cf7",
+    "url": "/static/js/main.b146d8d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
